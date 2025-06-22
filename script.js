a8199cd9a9606c247628e808cb79b8047ff7e6d9
Mohammad-Ali-0999 <alichoudhry068@gmail.com>
2025-06-22 18:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section) => {
+    section.classList.add('hidden');
+  });
+  document.getElementById(id).classList.remove('hidden');
+}
